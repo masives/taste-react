1390c5b81bd627b94132f3ddccf65496335953e6
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-03-02 21:05:52 +0100

    add button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import './App.css';
 // klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
 class App extends React.Component {
   render() {
-    return <div className="App">Hello world</div>;
+    return (
+      <div className="App">
+        Hello world
+        <button
+          onClick={() => {
+            console.log('siemka');
+          }}
+        >
+          kliknij mnie
+        </button>
+      </div>
+    );
   }
 }
 
