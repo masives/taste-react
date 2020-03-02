8c8adc2542364382f5022651c9dce7b79606cc28
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-03-02 20:54:21 +0100

    starting point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+// importujemy React by używać jsx i by móc użyć komponent
 import React from 'react';
-import logo from './logo.svg';
+// tak możemy importować style
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
+class App extends React.Component {
+  render() {
+    return <div className="App">Hello world</div>;
+  }
 }
 
 export default App;
