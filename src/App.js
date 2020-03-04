ff061f5ee24118f49fc4f8d4f60e23261885f8f8
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-03-04 18:55:20 +0100

    add button modyfing state

--- src/App.js
+++ src/App.js
@@ -6,19 +6,33 @@ import './App.css';
 // klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
 class App extends React.Component {
   state = {
-    counter: 10,
+    counter: 0,
   };
   render() {
-    console.log(this.state.counter);
     return (
       <div className="App">
         <p>Obecny stan licznika to: {this.state.counter}</p>
         <button
           onClick={() => {
-            console.log('Dodaję 1');
+            this.setState({ counter: this.state.counter - 1 });
+          }}
+        >
+          Odejmij 1
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ counter: this.state.counter + 1 });
           }}
         >
           Dodaj 1
+        </button>
+        <br />
+        <button
+          onClick={() => {
+            this.setState({ counter: this.state.counter * 2 });
+          }}
+        >
+          Podwój tego wariacika!
         </button>
       </div>
     );
