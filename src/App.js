cf94392e4a2b8777291f9cf1da588ebe877af80a
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-03-04 18:20:35 +0100

    add more jsx examples

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 // tak możemy importować style
 import './App.css';
 
+const message = 'Czekolada jest super!';
+const message1 = 'Programowanie jest fajne';
+const message2 = 'A ciastka smaczne';
+
 // klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
 class App extends React.Component {
   render() {
@@ -13,6 +17,23 @@ class App extends React.Component {
           <p>Powoli zaczyna to wchodzić</p>
           <b>Ale zaraz będzie grubo</b>
         </div>
+        <button
+          onClick={() => {
+            console.log('siemka');
+          }}
+        >
+          Cześć
+        </button>
+        <button
+          onClick={() => {
+            console.log('bajbaj');
+          }}
+        >
+          Żegnaj
+        </button>
+        <p>{message}</p>
+        <p>{message1}</p>
+        <p>{message2}</p>
       </div>
     );
   }
