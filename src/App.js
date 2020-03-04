d5e803e1486547a2fdc663bf5a0f5d8fac9ceb36
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-03-04 19:08:40 +0100

    add methods

--- src/App.js
+++ src/App.js
@@ -8,32 +8,27 @@ class App extends React.Component {
   state = {
     counter: 0,
   };
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decrementCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  doubleCounter = () => {
+    this.setState({ counter: this.state.counter * 2 });
+  };
+
   render() {
     return (
       <div className="App">
         <p>Obecny stan licznika to: {this.state.counter}</p>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter - 1 });
-          }}
-        >
-          Odejmij 1
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter + 1 });
-          }}
-        >
-          Dodaj 1
-        </button>
+        <button onClick={this.decrementCounter}>Odejmij 1</button>
+        <button onClick={this.incrementCounter}>Dodaj 1</button>
         <br />
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter * 2 });
-          }}
-        >
-          Podwój tego wariacika!
-        </button>
+        <button onClick={this.doubleCounter}>Podwój tego wariacika!</button>
       </div>
     );
   }
