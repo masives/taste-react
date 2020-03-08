fcadf0fc2dc2785ef667148f74b729f75c6ec2fb
Jozef Piecyk <jozollo@gmail.com>
2020-03-08 14:25:28 +0100

    add surnam to list

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,19 +3,31 @@ import React from 'react';
 class Searchbar extends React.Component {
   state = {
     name: '',
+    surname: '',
   };
 
   handleNameChange = (event) => {
     this.setState({ name: event.target.value });
   };
 
+  handleSurnameChange = (event) => {
+    this.setState({ surname: event.target.value });
+  };
+
   render() {
     console.log(this.state.name);
     return (
-      <label>
-        Imię
-        <input type="text" onChange={this.handleNameChange} value={this.state.name} />
-      </label>
+      <div>
+        <label>
+          Imię
+          <input type="text" onChange={this.handleNameChange} value={this.state.name} />
+        </label>
+        <br />
+        <label>
+          Nazwisko
+          <input type="text" onChange={this.handleSurnameChange} value={this.state.surname} />
+        </label>
+      </div>
     );
   }
 }
