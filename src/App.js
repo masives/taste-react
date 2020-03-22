4a9f810ba7f4dc6a618fd02347a8986b72309e20
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 10:40:07 +0100

    first encounter

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ import './App.css';
 // klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
 class App extends React.Component {
   render() {
-    return <div className="App">Hello world</div>;
+    return (
+      <div className="App">
+        <h1>Hello Skubnij React</h1>
+        <div>
+          <h1>Uczę się React</h1>
+          <p>Powoli zaczyna to wchodzić</p>
+          <b>Ale zaraz będzie grubo!</b>
+        </div>
+      </div>
+    );
   }
 }
 
 export default App;
+
+// 10:36
