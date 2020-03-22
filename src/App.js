a4670252bf713304fb9ed764226842c4a79f33e4
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 15:48:33 +0100

    extract users list to separate file

--- src/App.js
+++ src/App.js
@@ -5,41 +5,7 @@ import './App.css';
 import CounterButtons from './components/CounterButtons';
 import User from './components/User';
 import Searchbar from './components/Searchbar';
-
-class UsersList extends React.Component {
-  state = {
-    users: [],
-    loading: false,
-  };
-  componentDidMount = () => {
-    this.setState({ loading: true });
-    fetch('https://kuznia-kodu.pl/api/users')
-      // przerób odpowiedź na json
-      .then((data) => data.json())
-      .then((data) => {
-        const users = data.results;
-        this.setState({ users: users });
-      })
-      .finally(() => {
-        this.setState({ loading: false });
-      });
-  };
-
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        {this.state.users
-          .filter((user) => {
-            return user.first_name.includes(this.props.name) && user.last_name.includes(this.props.surname);
-          })
-          .map((user) => {
-            return <User key={user.id} name={user.first_name} surname={user.last_name} />;
-          })}
-      </div>
-    );
-  }
-}
+import UsersList from './components/UsersList';
 
 class App extends React.Component {
   state = {
---
+++ src/components/UsersList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import User from './User';
+
+class UsersList extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  componentDidMount = () => {
+    this.setState({ loading: true });
+    fetch('https://kuznia-kodu.pl/api/users')
+      // przerób odpowiedź na json
+      .then((data) => data.json())
+      .then((data) => {
+        const users = data.results;
+        this.setState({ users: users });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.users
+          .filter((user) => {
+            return user.first_name.includes(this.props.name) && user.last_name.includes(this.props.surname);
+          })
+          .map((user) => {
+            return <User key={user.id} name={user.first_name} surname={user.last_name} />;
+          })}
+      </div>
+    );
+  }
+}
+
+export default UsersList;
