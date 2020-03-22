318324d72e339267c250cff9e068a6743a02cadc
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 12:19:24 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class CounterButtons extends React.Component {
   render() {
     return (
       <div>
-        <h2>Obecnie na liczniku mamy: {this.state.counter}</h2>
+        <h2>Obecnie na liczniku mamy liczbę: {this.state.counter}</h2>
         <button onClick={this.incrementCounter}>Dodaj 1</button>
         <button onClick={this.decrementCounter}>Odejmij 1</button>
         <button onClick={this.doubleCounter}>Podwój</button>
@@ -35,31 +35,9 @@ class CounterButtons extends React.Component {
 // 12:14
 
 class App extends React.Component {
-  state = {
-    counter: 10,
-  };
-
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  decrementCounter = () => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  doubleCounter = () => {
-    this.setState({ counter: this.state.counter * 2 });
-  };
-
   render() {
-    console.log(this.state.counter);
     return (
       <div className="App">
-        <p>Obecny stan licznika to: {this.state.counter}</p>
-        <button onClick={this.incrementCounter}>Dodaj 1</button>
-        <button onClick={this.decrementCounter}>Odejmij 1</button>
-        <button onClick={this.doubleCounter}>Podwój</button>
-        <CounterButtons />
         <CounterButtons />
       </div>
     );
