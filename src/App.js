916c524d7fc8f7f9d86ea97bd47429c5e0fc251d
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 12:08:54 +0100

    define CounterButtons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react';
 // tak możemy importować style
 import './App.css';
 
-// klasa App (nazwa własna) dziedziczy od klasy Komponent, dzięki czemu mamy dostęp m.in. do funkcji render
+class CounterButtons extends React.Component {
+  state = {
+    counter: 8,
+  };
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Obecnie na liczniku mamy: {this.state.counter}</h2>
+        <button onClick={this.incrementCounter}>Dodaj 1</button>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   state = {
     counter: 10,
@@ -29,6 +47,8 @@ class App extends React.Component {
         <button onClick={this.incrementCounter}>Dodaj 1</button>
         <button onClick={this.decrementCounter}>Odejmij 1</button>
         <button onClick={this.doubleCounter}>Podwój</button>
+        <CounterButtons />
+        <CounterButtons />
       </div>
     );
   }
