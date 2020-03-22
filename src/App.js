1897787ce456d793bf74ae1b6cc01e2511a55dba
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 12:17:17 +0100

    move methods

--- src/App.js
+++ src/App.js
@@ -12,15 +12,27 @@ class CounterButtons extends React.Component {
     this.setState({ counter: this.state.counter + 1 });
   };
 
+  decrementCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  doubleCounter = () => {
+    this.setState({ counter: this.state.counter * 2 });
+  };
+
   render() {
     return (
       <div>
         <h2>Obecnie na liczniku mamy: {this.state.counter}</h2>
         <button onClick={this.incrementCounter}>Dodaj 1</button>
+        <button onClick={this.decrementCounter}>Odejmij 1</button>
+        <button onClick={this.doubleCounter}>Podwój</button>
       </div>
     );
   }
 }
+
+// 12:14
 
 class App extends React.Component {
   state = {
