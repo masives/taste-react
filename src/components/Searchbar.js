3c420aefeafaa6d956a9f1386aa9b54313aa354e
Jozef Piecyk <jozollo@gmail.com>
2020-03-22 13:16:25 +0100

    add searcbar state

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+// 13:20
+// 13:30
 class Searchbar extends React.Component {
+  state = {
+    name: '',
+  };
+
+  handleNameChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ name: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <label>
           Imię
-          <input type="text" />
+          <input type="text" value={this.state.name} onChange={this.handleNameChange} />
         </label>
+        <div>Obecny name to: {this.state.name}</div>
       </div>
     );
   }
